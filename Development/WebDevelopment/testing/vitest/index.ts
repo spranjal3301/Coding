af39ba319ba40917dfbe3cd32f3fb913edaf45e0
Pranjal <spranjal394@gmail.com>
2025-06-25 14:51:56 +0530

    ubuntu

--- Development/WebDevelopment/testing/vitest/index.ts
+++ Development/WebDevelopment/testing/vitest/index.ts
@@ -1,37 +1,3 @@
-//! Moving from jest to vitest (https://vitest.dev/)
- //* Vitest is the mildly recent entrant in the testing framework market.
-//- It has a bunch of benefits over jest, specially has great support for TS.
-//* So we’ll be moving to vitest for all future tests
-//* It is highly compatable with jest
-
-// Link to why vitest - https://vitest.dev/guide/why.html
-
-//- vistest is higly compatible with jest, so we can use most of the jest code as it is
-//- It has a lot of features like snapshot testing, mocking, and code coverage built-in
-//- It is designed to be fast and efficient, making it suitable for large projects
-//- It has a great community and ecosystem, with many plugins and integrations available 
-
-// npm i -D vitest
-
-//` When writing unit tests , you mock out all external service calls. 
-//- This means you test the core of your logic, and assume the database calls would succeed. 
-//- This is done so tests can run without starting a database / external services
-
-
-//` Mocking :
-//- Problem : What if I want to use the value that the database call returns?
-// vi.mock('../db', () => ({
-//   prismaClient: { sum: { create: vi.fn() }}
-// }));
-
-//`Deep mocking:
-// npm i -D vitest-mock-extended 
-//~ Create a __mocks__ directory in the same directory as the module you want to mock.
-// ```__mocks__/FileName 
-// export const prismaClient = mockDeep<PrismaClient>()
-
-// vi.mock('../db');
-
 import express from "express";
 import { z } from "zod";
 import { prismaClient } from "./db";
@@ -45,6 +11,67 @@ const sumInput = z.object({
 })
 
 
+app.post("/spy",async (req:any, res:any) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    //- spying the database call for testing purposes
+    //- This is just to ensure that the db call is made with correct inputs
+    //- In real world, you would not do this, as the db call is already
+    const rep = await prismaClient.sum.create({
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            result: answer
+        }
+    })
+    //! Uncomment the below code to see how the spy works
+    // await prismaClient.sum.create({
+    //     data: {
+    //         a: parsedResponse.data.a,
+    //         b: parsedResponse.data.a,
+    //         result: answer
+    //     }
+    // })
+
+    res.json({
+        answer,
+        id:rep.id
+    })
+});
+
+app.post("/mokeReturnValue",async (req:any, res:any) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    //- Mocking the database call for testing purposes
+    const rep = await prismaClient.sum.create({
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            result: answer
+        }
+    })
+
+    res.json({
+        answer,
+        id:rep.id
+    })
+});
 
 app.post("/mock",async (req:any, res:any) => {
     const parsedResponse = sumInput.safeParse(req.body)
@@ -70,8 +97,6 @@ app.post("/mock",async (req:any, res:any) => {
         answer
     })
 });
-
-
 
 app.post("/sum", (req:any, res:any) => {
     const parsedResponse = sumInput.safeParse(req.body)
--- Development/WebDevelopment/testing/vitest/test/index.test.ts
+++ Development/WebDevelopment/testing/vitest/test/index.test.ts
@@ -1,8 +1,9 @@
-import {describe, expect, test, it, vi} from 'vitest'; // @jest/globals highly compatible with jest
+import { describe, expect, test, it, vi } from "vitest"; // @jest/globals highly compatible with jest
 import request from "supertest";
 import { app } from "../index";
+import { prismaClient } from "../__mocks__/db"; //- require for moke return value
 
-//` When writing unit tests , you mock out all external service calls. 
+//` When writing unit tests , you mock out all external service calls.
 
 //` Mocking :
 //- Problem : What if I want to use the value that the database call returns?
@@ -12,64 +13,122 @@ import { app } from "../index";
 
 //`Deep Mocking (__mocks__/FileName.ts):
 //~ Create a __mocks__ directory in the same directory as the module you want to mock.
-vi.mock('../db');
+vi.mock("../db");
 
-describe("mocking extranal sevice(DB calls)", () => {
-    it("should return the sum of two numbers", async () => {
-        const res = await request(app).post("/mock").send({
-          a: 1,
-          b: 2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(3);
-      });
+describe("spying on methods inputs", () => {
+  it("should return the sum of two numbers", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 3,
+    });
 
-      it("should return the sum of two negative numbers", async () => {
-        const res = await request(app).post("/mock").send({
-          a: -1,
-          b: -2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(-3);
-      });
+    //- Spying the database call for testing purposes
+    vi.spyOn(prismaClient.sum, "create");
 
-      it("should return the sum of two zero number", async () => {
-        const res = await request(app).post("/mock").send({
-          a: 0,
-          b: 0
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(0);
-      });
+    const res = await request(app).post("/spy").send({
+      a: 1,
+      b: 2,
+    });
+
+
+    //- Check if the database call was made with correct inputs
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3,
+      },
+    });
+    //- Check if the database call was made once
+    expect(prismaClient.sum.create).toHaveBeenCalledTimes(1);
+
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
 });
 
+describe("mocking Return Value", () => {
+  it("should return the sum of two numbers", async () => {
+    //- Mocking external service return values
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 3,
+    });
 
+    const res = await request(app).post("/mokeReturnValue").send({
+      a: 1,
+      b: 2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
+
+  it("should return 411 if no inputs are provided", async () => {
+    const res = await request(app).post("/mokeReturnValue").send({});
+    expect(res.statusCode).toBe(411);
+    expect(res.body.message).toBe("Incorrect inputs");
+  });
+});
+
+describe("mocking extranal sevice(DB calls)", () => {
+  it("should return the sum of two numbers", async () => {
+    const res = await request(app).post("/mock").send({
+      a: 1,
+      b: 2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
+
+  it("should return the sum of two negative numbers", async () => {
+    const res = await request(app).post("/mock").send({
+      a: -1,
+      b: -2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(-3);
+  });
+
+  it("should return the sum of two zero number", async () => {
+    const res = await request(app).post("/mock").send({
+      a: 0,
+      b: 0,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(0);
+  });
+});
 
 describe("POST /sum", () => {
-    it("should return the sum of two numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 1,
-          b: 2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(3);
-      });
+  it("should return the sum of two numbers", async () => {
+    const res = await request(app).post("/sum").send({
+      a: 1,
+      b: 2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(3);
+  });
 
-      it("should return the sum of two negative numbers", async () => {
-        const res = await request(app).post("/sum").send({
-          a: -1,
-          b: -2
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(-3);
-      });
+  it("should return the sum of two negative numbers", async () => {
+    const res = await request(app).post("/sum").send({
+      a: -1,
+      b: -2,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(-3);
+  });
 
-      it("should return the sum of two zero number", async () => {
-        const res = await request(app).post("/sum").send({
-          a: 0,
-          b: 0
-        });
-        expect(res.statusCode).toBe(200);
-        expect(res.body.answer).toBe(0);
-      });
+  it("should return the sum of two zero number", async () => {
+    const res = await request(app).post("/sum").send({
+      a: 0,
+      b: 0,
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.answer).toBe(0);
+  });
 });
